9b4577c8057a8fb316ad1e27343abeb242bbe1b4
Hritik Kumar <130161281+ritik868@users.noreply.github.com>
2023-11-21 14:26:56 +0530

    Update optr_over.cpp

--- optr_over.cpp
+++ optr_over.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;   
+class abc{
+    int a = 7;
+      
+      public:
+    abc operator*(abc obj){
+        abc temp;
+        temp.a = a * obj.a;
+        return temp; 
+    }
+    
+    
+     abc operator-(abc obj){
+        abc temp;
+        temp.a = a - obj.a;
+        return temp; 
+    }
+    void show(){
+        cout<<a<<endl;
+    }
+};
 
+int main() {
+abc x,y,z,w;
+z = x*y;
+w = x-y;
+z.show();
+w.show();
+
+    return 0;
+}
